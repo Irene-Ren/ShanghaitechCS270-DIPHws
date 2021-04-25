0a09db6f647ca2ddb04f3e1fcef6cbc6c6f3ea1d
renyj <673542024@qq.com>
2021-04-25 23:50:02 +0800

    A unseparated version of watermarking and check watermark

--- hw2-任怡静/Q1_2.py
+++ hw2-任怡静/Q1_2.py
@@ -12,7 +12,6 @@ wrow,wcol = watermark.shape
 ratio = 1
 watermark = cv2.resize(watermark,(int(wcol/ratio),int(wrow/ratio)))
 wrow,wcol = int(wrow/ratio),int(wcol/ratio)
-print(watermark.shape)
 cv2.imshow("watermark",watermark.astype(np.uint8))
 cv2.waitKey(0)
 for i in range(watermark.shape[0]):
@@ -51,121 +50,93 @@ blocks = Cut_Y64Blocks(ycbcr[:,:,0])
 dctBlocks = DCTOnBlocks(blocks)
 
 qDctBlocks = (dctBlocks/Y_table).round().astype(np.int16)
-################################################
-def zigZag(block):
-    lines=[[] for i in range(8+8-1)] 
-    for y in range(8): 
-        for x in range(8): 
-            i=y+x 
-            if(i%2 ==0): 
-                lines[i].insert(0,block[y][x]) 
-            else:  
-                lines[i].append(block[y][x]) 
-    # print(lines)
-    return np.asarray([coefficient for line in lines for coefficient in line])
-
-
-def TruncateEndZeros(zigzagAC):
-    index = len(zigzagAC) - 1
-    truncated = []
-    while index >= 0:
-        if zigzagAC[index] != 0:
-            # print("FOUND: ",index)
-            break
-        index -= 1
-    for i in range(index+1):
-        truncated.append(zigzagAC[i])
-    return truncated
-
-def CollectSymbols(qDctBlocks):
-    symbols = []
-    for i in range(row_8):
-        for j in range(col_8):
-            subSym = TruncateEndZeros(zigZag(qDctBlocks[i][j])[1:])
-            for s in subSym:
-                if s != 0:
-                    symbols.append(s)
-    return symbols
-
-symbols = CollectSymbols(qDctBlocks)
-# print(symbols)
-print(len(symbols))
-def EncodeWatermark(originImg, watermark, alpha):
-    row, col, _ = originImg.shape
-    yChannel = originImg[:,:,0]
-    # print(row,col)
+# positives = []
+# for i in range(row_8):
+#     for j in range(col_8):
+#         for s in range(8):
+#             for t in range(8):
+#                 if qDctBlocks[i,j,s,t] != 0:
+#                     if not (s == 0 and t == 0):
+#                         positives.append(qDctBlocks[i,j,s,t])
+# print(positives)
+# print(len(positives))
+                
+def EncodeWatermarkBlocks(originYchannel, watermark, alpha):
     wrow,wcol = watermark.shape
     print(wrow,wcol)
 
-    oriDCT = np.zeros((row,col))
-    oriDCT[:,:] = dct(dct(yChannel.T, norm = 'ortho').T,norm = 'ortho')
+    blocks = Cut_Y64Blocks(originYchannel)
+    dctBlocks = DCTOnBlocks(blocks)
+    qDctBlocks = (dctBlocks/Y_table).round().astype(np.int16)
 
-    # oriDCT = yChannel # test code
+    # TODO: Need to make sure the value won't exceed, get all the non zero values and calculate the length, truncate k in range
+    copyDCT = qDctBlocks
+    k = 0
+    for i in range(row_8):
+        for j in range(col_8):
+            for s in range(8):
+                for t in range(8):
+                    if k >= wrow*wcol:
+                        return copyDCT
+                    if s >= 5 and t >= 4:
+                        break
+                    if qDctBlocks[i,j,s,t] != 0:
+                        if not (s == 0 and t == 0):
+                            copyDCT[i,j,s,t] = qDctBlocks[i,j,s,t] * (1 + alpha * watermark[k//wcol][k%wcol])
+                            k += 1
+    return None
+watermarkedBlocks = EncodeWatermarkBlocks(ycbcr[:,:,0],watermark,2)
 
-    acDCTlist = oriDCT.flatten()[1:]
-    kLargestIndices = np.argsort(acDCTlist,axis=None)[row*col-wrow*wcol-1 :][::-1]
-    # print(kLargestIndices)
-    # print(acDCTlist[kLargestIndices])
-    # print(len(kLargestIndices))
+def RebuildPicture(watermarkBlocks,YCbCrlayers):
+    row = row_8 * 8
+    col = col_8 * 8
+    blocks = np.zeros((row_8,col_8,8,8))
+    img = np.zeros((row,col,3))
+    for i in range(row_8):
+        for j in range(col_8):
+            dct_b = watermarkBlocks[i][j] * Y_table
+            blocks[i][j] = idct(idct(dct_b.T,norm = 'ortho').T,norm = 'ortho')
+    for i in range(row_8):
+        for j in range(col_8):
+            img[i*8:(i+1)*8,j*8:(j+1)*8,0] = blocks[i,j]
+    img[:,:,1] = YCbCrlayers[:,:,1]
+    img[:,:,2] = YCbCrlayers[:,:,2]
+    return img
 
-    copyDCT = oriDCT
-    for k in range(len(kLargestIndices)):
-        kvalue = kLargestIndices[k]+1
-        # print("DCT transform",kvalue//col,kvalue%col)
-        # print("watermark index",k//wrow,k%wcol)
-        copyDCT[kvalue//col][kvalue%col] = oriDCT[kvalue//col][kvalue%col] * (1 + alpha * watermark[k//wcol][k%wcol])
-
-    encryptedImg = np.zeros((row,col,3))
-    encryptedImg[:,:,0] = idct(idct(copyDCT.T, norm = 'ortho').T,norm = 'ortho')
-    encryptedImg[:,:,1] = originImg[:,:,1]
-    encryptedImg[:,:,2] = originImg[:,:,2]
-    
-    # print(np.max(encryptedImg))
-
-    # encryptedImg = copyDCT # test code
-
-    bgr = cv2.cvtColor(encryptedImg.astype(np.uint8), cv2.COLOR_YCrCb2BGR)
-    cv2.imwrite("EncryptedImage.tiff", bgr)
-    with open('coordinates.txt', 'w') as f:
-        for item in kLargestIndices:
-            f.write("%d," % item)
-    return encryptedImg,kLargestIndices
-
-# testarray = np.asarray([[10,10,7,6,5],[4,3,3,1,0]])
-# testmatrix = np.asarray([[10,2,3,4,3],[2,10,7,6,0],[10,7,6,5,3],[1,1,1,0,0],[4,4,2,3,10]])
-# # testmatrix = np.asarray([[10,10,10,10,10],[10,10,10,10,10],[10,10,10,10,10],[10,10,10,10,10],[10,10,10,10,10]])
-# encryptedImg, positionDict = EncodeWatermark(testmatrix, testarray,2)
-# print(encryptedImg)
-# print(positionDict)
-
-encryptedImg, positionDict = EncodeWatermark(ycbcr, watermark,0.2)
-print(encryptedImg.shape)
-
+encryptedImg = RebuildPicture(watermarkedBlocks, ycbcr)
 
 cv2.imshow("Origin",ycbcr.astype(np.uint8))
 cv2.waitKey(0)
 cv2.imshow("Encrypted image",encryptedImg.astype(np.uint8))
 cv2.waitKey(0)
 
-def DecodeWatermark(originImg, encryptedImg,kLargestIndices,wrow,wcol,alpha):
-    row, col, _ = encryptedImg.shape
-    yChannel_e = encryptedImg[:,:,0]
-    yChannel_o = originImg[:,:,0]
-    # print(row,col)
+def DecodeWatermark(originYchannel, encryptedYchannel,wrow,wcol,alpha):
     watermark = np.zeros((wrow,wcol))
 
-    oriDCT = np.zeros((row,col))
-    oriDCT[:,:] = dct(dct(yChannel_o.T, norm = 'ortho').T,norm = 'ortho')
-    encryptDCT = np.zeros((row,col))
-    encryptDCT[:,:] = dct(dct(yChannel_e.T, norm = 'ortho').T,norm = 'ortho')
-    for k in range(len(kLargestIndices)):
-        kvalue = kLargestIndices[k]+1
-        # print("DCT transform",kvalue//col,kvalue%col)
-        # print("watermark index",k//wrow,k%wcol)
-        watermark[k//wcol][k%wcol] = (encryptDCT[kvalue//col][kvalue%col] / oriDCT[kvalue//col][kvalue%col] - 1) / alpha
-    return watermark
+    blocks_o = Cut_Y64Blocks(originYchannel)
+    dctBlocks_o = DCTOnBlocks(blocks_o)
+    qDctBlocks_o = (dctBlocks_o/Y_table).round().astype(np.int16)
 
-extractedWM = DecodeWatermark(ycbcr,encryptedImg,positionDict,wrow,wcol,0.2)
+    blocks_e = Cut_Y64Blocks(encryptedYchannel)
+    dctBlocks_e = DCTOnBlocks(blocks_e)
+    qDctBlocks_e = (dctBlocks_e/Y_table).round().astype(np.int16)
+
+    k = 0
+    for i in range(row_8):
+        for j in range(col_8):
+            for s in range(8):
+                for t in range(8):
+                    if k >= wrow*wcol:
+                        return watermark
+                    if s >= 5 and t >= 4:
+                        break
+                    if qDctBlocks_e[i,j,s,t] != 0:
+                        if not (s == 0 and t == 0):
+                            watermark[k//wcol][k%wcol] = (qDctBlocks_e[i,j,s,t] / qDctBlocks_o[i,j,s,t] - 1) / alpha
+                            k += 1
+    return None
+
+extractedWM = DecodeWatermark(ycbcr[:,:,0],encryptedImg[:,:,0],wrow,wcol,2)
 
 # encryptimg = cv2.imread("DecompressedImage.tiff")
 # encryptycbcr = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
@@ -184,6 +155,8 @@ extractedWM = DecodeWatermark(ycbcr,encryptedImg,positionDict,wrow,wcol,0.2)
 for i in range(extractedWM.shape[0]):
     for j in range(extractedWM.shape[1]):
         extractedWM[i,j] *= 255
+        if extractedWM[i,j] > 255:
+            extractedWM[i,j] = 255
 extractedWM = cv2.resize(extractedWM,(wcol*ratio,wrow*ratio))
 cv2.imshow("ExtractedWm",extractedWM.astype(np.uint8))
 cv2.waitKey(0)
