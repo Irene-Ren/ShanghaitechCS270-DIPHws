a7ad3965a5da9301fa88c143f6d75a13d502bbe1
renyj <673542024@qq.com>
2021-05-05 05:17:16 +0800

    Some ideas about extraction
    To use vision.CascadeObjectDetector('eyepairsmall') sort of things to only detect facial infos, then merge all the lists of detections may work

--- hw2-任怡静/Q2_1.py
+++ hw2-任怡静/Q2_1.py
@@ -1,3 +1,26 @@
+import cv2
+import sys
 import numpy as np
-from scipy.sparse import linalg as linalg
-from scipy.sparse import lil_matrix as lil_matrix
+
+imagePath = sys.argv[1]
+
+image = cv2.imread(imagePath)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+faces = faceCascade.detectMultiScale(
+    gray,
+    scaleFactor=1.3,
+    minNeighbors=3,
+    minSize=(30, 30)
+)
+
+print("[INFO] Found {0} Faces!".format(len(faces)))
+
+image_only_face = np.zeros(image.shape)
+for (x, y, w, h) in faces:
+    # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    image_only_face[x:x+w, y:y+h,:] = image[x:x+w, y:y+h,:]
+
+status = cv2.imwrite('faces_detected.jpg', image_only_face)
+print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
