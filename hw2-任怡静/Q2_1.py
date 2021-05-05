3c67adac594bc01478c77e3741c367687d26aeb0
renyj <673542024@qq.com>
2021-05-06 00:43:20 +0800

    A failed python version, unless find sth to match 9 features

--- hw2-任怡静/Q2_1.py
+++ hw2-任怡静/Q2_1.py
@@ -1,26 +1,22 @@
 import cv2
 import sys
 import numpy as np
+from scipy.io import loadmat
 
-imagePath = sys.argv[1]
-
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.3,
-    minNeighbors=3,
-    minSize=(30, 30)
-)
-
-print("[INFO] Found {0} Faces!".format(len(faces)))
-
-image_only_face = np.zeros(image.shape)
-for (x, y, w, h) in faces:
-    # cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    image_only_face[x:x+w, y:y+h,:] = image[x:x+w, y:y+h,:]
-
-status = cv2.imwrite('faces_detected.jpg', image_only_face)
-print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
+girl_img = cv2.imread("hw2_files\Q2\girl.jpeg")
+man_img = cv2.imread("hw2_files\Q2\man.jpg")
+girl_features = loadmat("hw2_files\Q2\girl_features.mat")["girl_features"]
+girl_features = np.float32(girl_features)
+man_features = loadmat("hw2_files\Q2\man_features.mat")["man_features"]
+man_features = np.float32(man_features)
+M = cv2.getAffineTransform(man_features[0:3], girl_features[0:3])
+man_trans = np.zeros(girl_img.shape)
+grow,gcol,_ = girl_img.shape
+for i in range(3):
+    man_trans[:,:,i] = cv2.warpAffine(man_img[:,:,i], M, (gcol,grow))
+cv2.imshow("Pic",np.uint8(man_trans))
+cv2.waitKey(0)
+C = cv2.addWeighted(np.uint8(girl_img), 0.5, np.uint8(man_trans), 0.5, 0.0)
+cv2.imshow("Answer",C)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
